adb06bc3a82a7b98ff8a4711a1f8d425f3b8d79b
Lena Jeremiah <49532117+Jeremiahjacinth13@users.noreply.github.com>
2021-11-24 06:30:05 +0100

    set up realtime database

--- src/ChatScreen.tsx
+++ src/ChatScreen.tsx
@@ -1,16 +1,36 @@
 import React, { useEffect, useState } from 'react'
-import { Pressable, StyleSheet, Text, View } from 'react-native'
+import { Alert, FlatList, Pressable, StyleSheet, Text, View } from 'react-native'
 import { useNavigation } from '@react-navigation/core'
+import { ScrollView, TextInput } from 'react-native-gesture-handler'
 
 import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth'
-import { ScrollView } from 'react-native-gesture-handler'
+import db from '@react-native-firebase/database'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+
+type Message = {
+    text: string
+    createdAt: number
+    senderId: string
+}
 
 export const ChatScreen = () => {
     const [user, setUser] = useState<FirebaseAuthTypes.User>()
+    const [messages, setMessages] = useState<any>([])
 
     const navigator = useNavigation()
 
     useEffect(() => {
+
+        const getMessage = async () => {
+            const dbMessages = await (await db().ref('messages').limitToFirst(10).once('value')).val()
+            setMessages(Object.values(dbMessages))
+        }
+
+        db().ref('/messages').on('child_added', snapshot => {
+            setMessages(messages => [...messages, snapshot.val()])
+        })
+
         const authChangeSubscriber =
             auth()
                 .onAuthStateChanged(user => {
@@ -18,18 +38,34 @@ export const ChatScreen = () => {
                     else navigator.navigate('Login' as never)
                 })
 
+        // getMessage()
+
         return authChangeSubscriber
     }, [])
+
+    const sendMessage = (e) => {
+        const newMessage = db().ref('messages').push({text: e.nativeEvent.text, senderId: user?.uid, createdAt: Date.now() })
+        console.log(e.nativeEvent.text)
+    }
 
     const handleLogout = () => {
         auth().signOut()
     }
 
     return (
-        <ScrollView style = {{flex: 1}}>
+        <SafeAreaView style={{ flex: 1 }}>
             <Pressable onPress={handleLogout}><Text>Logout</Text></Pressable>
-            <Text>{JSON.stringify(user, null, 3)}</Text>
-        </ScrollView>
+            <FlatList
+                keyExtractor={(item) => item.createdAt}
+                data={messages}
+                renderItem={({ item }) => <Text>{item.text}</Text>}
+            />
+            <TextInput
+                placeholder='Send message'
+                onSubmitEditing={sendMessage}
+            />
+
+        </SafeAreaView>
     )
 }
 
